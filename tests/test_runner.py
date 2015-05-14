76a92ebf9561f29bad29494ce8cdac823b743d54
David Cardozo <gd.cardozo684@uniandes.edu.co>
2015-05-14 00:35:05 -0500

    Creating git folder

---
+++ tests/test_runner.py
@@ -0,0 +1,31 @@
+import unittest
+
+import test_recompose
+import test_find_basis
+import test_utils
+import test_matrix_ln
+import test_diagonalize
+import test_compose
+import test_similarity_matrix
+import test_simplify
+import test_operator
+import test_hypersphere
+import test_trig
+import test_rotate
+
+loader = unittest.TestLoader()
+
+suite = unittest.TestSuite()
+suite.addTest(test_recompose.get_suite())
+suite.addTest(test_find_basis.get_suite())
+suite.addTest(test_utils.get_suite())
+suite.addTest(test_trig.get_suite())
+suite.addTest(test_rotate.get_suite())
+suite.addTest(test_matrix_ln.get_suite())
+suite.addTest(test_diagonalize.get_suite())
+suite.addTest(test_compose.get_suite())
+suite.addTest(test_similarity_matrix.get_suite())
+suite.addTest(test_simplify.get_suite())
+suite.addTest(test_operator.get_suite())
+suite.addTest(test_hypersphere.get_suite())
+unittest.TextTestRunner(verbosity=2).run(suite)
